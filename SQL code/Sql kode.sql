5b7c695ab5926d28af222536e648cc002143fec8
Yaimee <37310514+Yaimee@users.noreply.github.com>
2022-06-01 11:50:50 +0200

    Rasmus push: ændringer i staff

--- SQL code/Sql kode.sql
+++ SQL code/Sql kode.sql
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXIST `beer` (
   procent double NOT NULL,
   origin varchar(45) NOT NULL,
   description varchar(200) NOT NULL,
-  imgURL varchar(2000) NOT NULL,
+  image_URL varchar(2000) NOT NULL,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -27,7 +27,7 @@ CREATE TABLE IF NOT EXIST `vin` (
   procent varchar(45) NOT NULL,
   origin varchar(45) NOT NULL,
   description varchar(45) NOT NULL,
-  imgURL varchar(2000) NOT NULL,
+  image_URL varchar(2000) NOT NULL,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -43,7 +43,7 @@ CREATE TABLE IF NOT EXIST `uden_alkohol` (
   procent varchar(45) NOT NULL,
   origin varchar(45) NOT NULL,
   description varchar(200) NOT NULL,
-  imgURL varchar(2000) NOT NULL,
+  image_URL varchar(2000) NOT NULL,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -54,7 +54,7 @@ CREATE TABLE IF NOT EXIST `spiritus` (
   procent varchar(45) NOT NULL,
   origin varchar(45) NOT NULL,
   description varchar(200) NOT NULL,
-  imgURL varchar(2000) NOT NULL,
+  image_URL varchar(2000) NOT NULL,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -65,7 +65,7 @@ CREATE TABLE IF NOT EXIST `mad` (
   procent varchar(45) NOT NULL,
   origin varchar(45) NOT NULL,
   description varchar(200) NOT NULL,
-  imgURL varchar(2000) NOT NULL,
+  image_URL varchar(2000) NOT NULL,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -75,6 +75,16 @@ CREATE TABLE IF NOT EXIST `events` (
   date date NOT NULL,
   time varchar(45) NOT NULL,
   info varchar(200) NOT NULL,
-  imgURL varchar(2000) NOT NULL,
+  image_URL varchar(2000) NOT NULL,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `tjili-pop`.`beer` (`id`,`name`,`price`,`procent`,`origin`,`description`,`image_URL`) VALUES (2,'Stout',NULL,8,'England','A fine brew','https://i.imgur.com/wpFtGjP.jpg');
+INSERT INTO `tjili-pop`.`beer` (`id`,`name`,`price`,`procent`,`origin`,`description`,`image_URL`) VALUES (5,'Guiness',NULL,8,'England','A fine brew','https://i.imgur.com/qg0BRdt.jpg');
+INSERT INTO `tjili-pop`.`beer` (`id`,`name`,`price`,`procent`,`origin`,`description`,`image_URL`) VALUES (9,'Tuborg',NULL,4.5,'Denmark','A good brew','https://i.imgur.com/tnrI7Cz.jpg');
+
+INSERT INTO `tjili-pop`.`events` (`id`,`name`,`date`,`time`,`info`,`image_URL`) VALUES (1,'Metallica','2022-05-11','18:00','Det bliver kanon','https://i.imgur.com/QC58WTX.jpg');
+INSERT INTO `tjili-pop`.`events` (`id`,`name`,`date`,`time`,`info`,`image_URL`) VALUES (2,'Kiss','2022-05-10','18:16','De spiller godt','https://i.imgur.com/AEGG8kA.jpg');
+INSERT INTO `tjili-pop`.`events` (`id`,`name`,`date`,`time`,`info`,`image_URL`) VALUES (3,'Led Zeppelin','2022-05-12','12:30','De er bare så gode','https://i.imgur.com/Azts2Ro.jpg');
+INSERT INTO `tjili-pop`.`events` (`id`,`name`,`date`,`time`,`info`,`image_URL`) VALUES (4,'The Wailers','2022-07-12','15:40','Hold da op vi glæder os','https://i.imgur.com/KQSRFWA.jpg');
+INSERT INTO `tjili-pop`.`events` (`id`,`name`,`date`,`time`,`info`,`image_URL`) VALUES (5,'Die Antwort','2022-05-12','14:30','Det bliver fjong','https://i.imgur.com/fBzfEkA.jpg');
